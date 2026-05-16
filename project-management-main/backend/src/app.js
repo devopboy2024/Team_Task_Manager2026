ecb575a06d6f74eb1ecf15dfcd6dd0262f5d3369
devopboy2024 <devil2003shubham@gmail.com>
2026-05-16 13:08:05 +0530

    made changes docker file  and app.js

--- project-management-main/backend/src/app.js
+++ project-management-main/backend/src/app.js
@@ -65,6 +65,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
     console.log('Database connected successfully');
 
     
